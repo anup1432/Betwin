a4c61fbd4e631df9b84fd9ed1485ed2339f125d0
anup1432 <140874193+anup1432@users.noreply.github.com>
2025-08-20 10:42:18 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
-  password: String,
-  wallet: { type: Number, default: 0 }
-});
-module.exports = mongoose.model('User', UserSchema);
+  password: { type: String },
+  avatar: { type: String },
+  balance: { type: Number, default: 100 },
+  totalProfit: { type: Number, default: 0 },
+  totalBets: { type: Number, default: 0 },
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  suspended: { type: Boolean, default: false }
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
