5a720d714b219005dd42cb72f7b6e1fce25ac069
anup1432 <140874193+anup1432@users.noreply.github.com>
2025-08-21 10:24:26 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,8 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-export const cfg = {
-  PORT: process.env.PORT || 10000,
-  CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
+
+export default {
+  PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
-  ADMIN_USER: process.env.ADMIN_USER,
-  ADMIN_PASS: process.env.ADMIN_PASS,
-  DEFAULT_DURATION: Number(process.env.BATTLE_DEFAULT_DURATION || 60),
-  SYMBOLS: (process.env.SYMBOLS || 'BTCUSDT').split(','),
-  TELEGRAM_LINK: process.env.TELEGRAM_LINK || ''
+  JWT_SECRET: process.env.JWT_SECRET || "secret123",
 };
