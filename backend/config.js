b28ad6ef55a5235702c17806cbdf5cb71a1198c6
anup1432 <140874193+anup1432@users.noreply.github.com>
2025-08-21 12:10:27 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export default {
+export const cfg = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET || "secret123",
