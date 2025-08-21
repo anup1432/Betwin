390c9c434d2e9bd1093bc5ae91e39422c426fa68
anup1432 <140874193+anup1432@users.noreply.github.com>
2025-08-21 08:52:33 +0530

    Delete backend/routes directory

--- backend/routes/server.js
+++
@@ -1,8 +0,0 @@
-const userRoutes = require('./routes/user');
-app.use('/api/auth', userRoutes);
-
-const betRoutes = require('./routes/bet');
-app.use('/api/bet', betRoutes);
-
-const walletRoutes = require('./routes/wallet');
-app.use('/api/wallet', walletRoutes);
