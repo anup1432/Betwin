bf90453bf3f724e47148cef8efd9abd53695f469
anup1432 <140874193+anup1432@users.noreply.github.com>
2025-08-21 12:03:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,10 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./db.js";
-
-import authRoutes from "./routes/auth.js";
-import battleRoutes from "./routes/battles.js";
-import adminRoutes from "./routes/admin.js";
 import walletRoutes from "./routes/wallet.js";
 
 const app = express();
-
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Connect Database
-connectDB();
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/battles", battleRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/wallet", walletRoutes);
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("✅ BetWin Backend Running...");
-});
-
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+connectDB
